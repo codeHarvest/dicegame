d9ff06299065c6bdc701c28416cfeb3d512efa79
codeHarvest <madhusudanbhardwaj94@gmail.com>
2020-01-19 22:15:19 +0530

    init function and checking if game is on

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
-/*
-GAME RULES:
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+var scores,roundScore,activePlayer,isGameOn;
 
-*/
-
-var scores,roundScore,activePlayer;
-
-scores = [0,0];
-
-roundScore = 0;
-
-activePlayer = 0;
-
+init();
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -36,38 +21,72 @@ function nextPlayer(){
 
 function rollDice(){
     
-    var diceValue = Math.floor(Math.random() * 6 ) + 1;
+    if(isGameOn){
+        var diceValue = Math.floor(Math.random() * 6 ) + 1;
 
-    var dice = document.querySelector('.dice');
+        var dice = document.querySelector('.dice');
 
+        dice.style.display = 'block'; 
+        
+        dice.src = `dice-${diceValue}.png`;
     
-    dice.style.display = 'block'; 
-    
-    dice.src = `dice-${diceValue}.png`;
- 
-    if(diceValue !== 1){
-        roundScore += diceValue;
+        if(diceValue !== 1){
+            roundScore += diceValue;
 
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
 
-    }else{
-        nextPlayer();
+        }else{
+            nextPlayer();
+        }
     }
 }
 
 function holdScore(){
-    scores[activePlayer] += roundScore;
+    if(isGameOn){
+        scores[activePlayer] += roundScore;
 
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-    if(scores[activePlayer] >= 20){
-        document.querySelector('#name-' + activePlayer).textContent = 'winner!';
-        document.querySelector('.dice').style.display = 'none';
-        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+        if(scores[activePlayer] >= 100){
+            document.querySelector('#name-' + activePlayer).textContent = 'winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            isGameOn = false;
+        }
     }
 }
+
+function init(){
+
+    scores = [0,0];
+
+    roundScore = 0;
+
+    activePlayer = 0;
+
+    isGameOn = true;
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+            
+    
+    var player1 = prompt('Before we start the game enter your name Player 1');
+    var player2 = prompt('Player 2 enter your name');
+
+    document.querySelector('#name-0').textContent = player1 && player1 ? player1 :  'player1';
+    document.querySelector('#name-1').textContent = player2 && player2 ? player2 :  'player2';
+}
+
 
 document.querySelector('.btn-roll').addEventListener('click', rollDice);
 
 document.querySelector('.btn-hold').addEventListener('click', holdScore);
+
+document.querySelector('.btn-new').addEventListener('click', init);
